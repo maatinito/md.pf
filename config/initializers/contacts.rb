87ff5fa949ccffb0b5c3c2dbe6048b8e3d4c0aeb
maatinito <15379878+maatinito@users.noreply.github.com>
2018-10-26 14:38:09 -1000

    Polynesia contacts (dev)

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,9 +1,9 @@
 if !defined?(CONTACT_EMAIL)
-  CONTACT_EMAIL = "contact@demarches-simplifiees.fr"
-  EQUIPE_EMAIL = "equipe@demarches-simplifiees.fr"
-  TECH_EMAIL = "tech@demarches-simplifiees.fr"
+  CONTACT_EMAIL = "clautier@idt.pf"
+  EQUIPE_EMAIL = "clautier@idt.pf"
+  TECH_EMAIL = "clautier@idt.pf"
 
-  CONTACT_PHONE = "01 76 42 02 87"
+  CONTACT_PHONE = "89 54 05 68"
 
   OLD_CONTACT_EMAIL = "contact@tps.apientreprise.fr"
 end
