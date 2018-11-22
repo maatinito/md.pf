3702888de6e8a2b3af93d366769f38cc78c452ed
maatinito <15379878+maatinito@users.noreply.github.com>
2018-11-22 09:16:40 -1000

    Use sysadmin as first contact for pf

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,4 +1,16 @@
 if !defined?(CONTACT_EMAIL)
+  CONTACT_EMAIL = "sysadmin@informatique.gov.pf"
+  EQUIPE_EMAIL = "sysadmin@informatique.gov.pf"
+  TECH_EMAIL = "sysadmin@informatique.gov.pf"
+
+  CONTACT_PHONE = "40 54 43 54"
+
+  OLD_CONTACT_EMAIL = "contact@tps.apientreprise.fr"
+end
+
+#----- in case previous if is commented
+
+if !defined?(CONTACT_EMAIL)
   CONTACT_EMAIL = "clautier@idt.pf"
   EQUIPE_EMAIL = "clautier@idt.pf"
   TECH_EMAIL = "clautier@idt.pf"
--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,7 @@ Mailjet.configure do |config|
   config.api_key = Rails.application.secrets.mailjet[:api_key]
   config.secret_key = Rails.application.secrets.mailjet[:secret_key]
   config.default_from = CONTACT_EMAIL
-  puts 'clé mailjet'
+  puts "MAILJET_SECRET_KEY=#{ENV.fetch('MAILJET_SECRET_KEY')}"
   puts "APP_HOST=#{ENV.fetch('APP_HOST')}"
 
 end
